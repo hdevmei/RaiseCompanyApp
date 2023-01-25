a4ceb1a9e02ebceef0ffb158c367ae1421d08c47
hdevmei <hdevmei@gmail.com>
2023-01-25 23:56:38 +0100

    -

--- RaiseCompanyApp/Controllers/AddEstablishmentViewController.swift
+++ RaiseCompanyApp/Controllers/AddEstablishmentViewController.swift
@@ -2,28 +2,18 @@
 //  AddEstablishmentViewController.swift
 //  RaiseCompanyApp
 //
-//  Created by mei_yocontrolo on 16/01/2023.
+//  Created by mei_yocontrolo on 25/01/2023.
 //
 
+import Foundation
 import UIKit
 
-class AddEstablishmentViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+class AddEstablishmentViewController: UIViewController{
+    
+    
+    @IBAction func cancelBtn(_ sender: Any) {
+        self.dismiss(animated: true, completion: nil)
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
