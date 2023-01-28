595a9ccf5a5e328ae3c7121a2b76edf7528bc4ea
hdevmei <hdevmei@gmail.com>
2023-01-29 00:39:31 +0100

    some textfields to add establishments

--- RaiseCompanyApp/Controllers/AddEstablishmentViewController.swift
+++ RaiseCompanyApp/Controllers/AddEstablishmentViewController.swift
@@ -7,13 +7,73 @@
 
 import Foundation
 import UIKit
+import Alamofire
 
 
 class AddEstablishmentViewController: UIViewController{
     
+    let url = URL(string: "http://127.0.0.1:5000/company/addEstablishment")!
+
+    var canSaveEstablishment: Bool = false
+    
+    @IBOutlet weak var toastMessage: UILabel!
+    
+    @IBOutlet weak var locationTextField: UITextField!
+    
+    @IBOutlet weak var benefitsTextField: UITextField!
+    
+    @IBOutlet weak var lossesTextField: UITextField!
+    
+    @IBOutlet weak var scheduleTextField: UITextField!
+    
+    let params: Parameters = [
+        "benefits": 2000,
+        "losses": 1500,
+        "schedule": "11:00 - 19:00",
+        "location": "FANTASAMA",
+    ]
     
     @IBAction func cancelBtn(_ sender: Any) {
         self.dismiss(animated: true, completion: nil)
     }
+    
+    
+    @IBAction func addEstablishmentBtn(_ sender: UIButton) {
+        print("apretado")
+        checkTextfields()
+        if canSaveEstablishment == true{
+//          do post 
+            DataManager.postMethod(url: url, params: params)
+            self.dismiss(animated: true, completion: nil)
+        } else if canSaveEstablishment == false{
+            print("faltan campos")
+//            showToast()
+        }
+        
+      
+
+    }
+    
+    
+    
+    
+    func checkTextfields(){
+        if locationTextField.text == ""{
+            canSaveEstablishment = false
+        }else if locationTextField.text != ""{
+            canSaveEstablishment = true
+        }
+    }
+    
+    /*
+    func showToast(){
+        toastMessage.text = "Need to complete location and schedule"
+        UIView.animate(withDuration: 4.0, delay: 0.1, options: .curveEaseOut, animations: {
+            self.toastMessage.alpha = 0.0
+        }, completion: {(isCompleted) in
+            self.toastMessage.removeFromSuperview()
+        })
+    }
+    */
     
 }
--- RaiseCompanyApp/Model/DataManager.swift
+++ RaiseCompanyApp/Model/DataManager.swift
@@ -6,4 +6,46 @@
 //
 
 import Foundation
+import Alamofire
+
+
+
+class DataManager: UIViewController{
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print("holaaa")
+        // Do any additional setup after loading the view.
+    }
+    
+    
+    @IBAction func btnPrueba(_ sender: UIButton) {
+    }
+    
+    @IBAction func btnGet(_ sender: UIButton) {
+    }
+    
+    
+    @IBAction func btnDelete(_ sender: UIButton) {
+    }
+    
+    
+    
+    
+    static func postMethod(url: URL, params:Parameters) {
+            AF.request(url, method: .post, parameters: params, encoding: JSONEncoding.default)
+            .validate(statusCode: 200..<300)
+            .responseJSON { response in
+                switch response.result {
+                case .success(let value):
+                    let json = value as! [String: Any]
+                    print(json)
+                case .failure(let error):
+                    print(error)
+                }
+            }
+    }
+}
+
 
