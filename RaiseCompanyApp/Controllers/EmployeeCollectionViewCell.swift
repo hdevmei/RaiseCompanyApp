c4b127d85689f94df663d621ff18406b82d0e07c
hdevmei <hdevmei@gmail.com>
2023-02-01 20:58:06 +0100

    -

---
+++ RaiseCompanyApp/Controllers/EmployeeCollectionViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  EmployeeCollectionViewCell.swift
+//  RaiseCompanyApp
+//
+//  Created by mei_yocontrolo on 01/02/2023.
+//
+
+import UIKit
+
+class EmployeeCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var employeeImage: UIImageView!
+    
+    @IBOutlet weak var nameEmployee: UILabel!
+}
--- RaiseCompanyApp/Controllers/EstablishmentDetailViewController.swift
+++ RaiseCompanyApp/Controllers/EstablishmentDetailViewController.swift
@@ -8,6 +8,26 @@
 import Foundation
 import UIKit
 
-class EstablishmentDetailedViewController: UIViewController{
+class EstablishmentDetailedViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource{
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 8
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "roundedEmployeeCell", for: indexPath) as! EmployeeCollectionViewCell
+        return cell
+    }
+    
+    
+    @IBOutlet weak var employeesCollectionView: UICollectionView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        employeesCollectionView.dataSource = self
+        employeesCollectionView.delegate = self
+        // Do any additional setup after loading the view.
+    }
+    
+    
     
 }
