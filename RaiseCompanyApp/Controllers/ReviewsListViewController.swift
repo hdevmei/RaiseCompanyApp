539a6f2a46a0a339428ad2f79bca98dc97fd9904
hdevmei <hdevmei@gmail.com>
2023-02-02 11:14:06 +0100

    Reviews scroll

---
+++ RaiseCompanyApp/Controllers/ReviewsListViewController.swift
@@ -0,0 +1,46 @@
+//
+//  ReviewsListViewController.swift
+//  RaiseCompanyApp
+//
+//  Created by mei_yocontrolo on 02/02/2023.
+//
+
+import UIKit
+
+class ReviewsListViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    @IBOutlet weak var reviewsTable: UITableView!
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 40
+        
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "reviewCell", for: indexPath)
+        return cell
+    }
+    
+
+
+    @IBOutlet weak var reviewsTableView: UITableView!
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        reviewsTable.delegate = self
+        reviewsTable.dataSource = self
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ RaiseCompanyApp/Model/Domain/Review.swift
@@ -0,0 +1,19 @@
+//
+//  Review.swift
+//  RaiseCompanyApp
+//
+//  Created by mei_yocontrolo on 02/02/2023.
+//
+
+import Foundation
+import UIKit
+
+
+class Review : Codable {
+    let id_review: Int
+    let raintg: Float
+    let date: String
+    let description: String
+    let id_establishment: Int
+    let id_client: Int
+}
